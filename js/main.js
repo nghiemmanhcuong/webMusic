9dee040347c0761308fae8e9e5f964258f843dbc
nghiemmanhcuong <nghiemmanhcuong98@gmail.com>
2021-10-27 10:55:41 +0700

    update cource code

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ const prevBtn = document.getElementById('prev-slide');
 const nextBtn = document.getElementById('next-slide');
 const sliderMain = document.querySelector('.featured__main');
 const sliders = document.querySelectorAll('.slider__item');
+const headerNavbarOpen = document.getElementById('header-nav-open');
+const headerNavbarclose = document.getElementById('header-nav-close');
+const headerNavbar = document.querySelector('.header__navbar');
 let index = 0;
 
 nextBtn.onclick = function(){
@@ -22,3 +25,12 @@ prevBtn.onclick = function(){
 setInterval(function(){
     nextBtn.click();
 },10000)
+
+headerNavbarOpen.onclick = function(){
+    headerNavbar.classList.add('active');
+    headerNavbarclose.style.display = 'block';
+}
+headerNavbarclose.onclick = function(){
+    headerNavbarclose.style.display = 'none';
+    headerNavbar.classList.remove('active');
+}
